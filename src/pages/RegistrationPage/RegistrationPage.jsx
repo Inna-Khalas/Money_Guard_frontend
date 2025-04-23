1ba2fba56f95ab13206109bb51a08a444c20711d
O. HADALIN <dyblikat777@gmail.com>
2025-04-23 17:24:03 +0300

    start mob css

--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,10 +1,20 @@
+import styles from './RegistrationPage.module.css';
 import RegistrationForm from '../../components/RegistrationForm/RegistrationForm.jsx';
 
 const RegistrationPage = () => {
   return (
-    <div>
-      <h2>Register</h2>
-      <RegistrationForm />
+    <div className={styles.pageWrapper}>
+      <div className={styles.background}></div>
+
+      <div className={styles.container}>
+        <div className={styles.logoWrapper}>
+          <span className={styles.logoAccent}>M</span>oneyGuard
+        </div>
+
+        <h1 className={styles.title}>Sign Up</h1>
+
+        <RegistrationForm />
+      </div>
     </div>
   );
 };

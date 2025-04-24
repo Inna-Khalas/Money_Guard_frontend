519ae9dc65998d405976fe527f8929b9c6712e94
Alena-Solo <bazarkina.alena2020@gmail.com>
2025-04-24 13:09:22 +0200

    loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import { useEffect, useState } from 'react';
 
 import RegistrationPage from '../pages/RegistrationPage/RegistrationPage';
 import LoginPage from '../pages/LoginPage/LoginPage';
@@ -10,10 +11,30 @@ import NotFound from '../pages/NotFound';
 import HomeTab from '../pages/HomeTab/HomeTab';
 import Balance from './Balance/Balance';
 import DashboardPage from '../pages/DashboardPage/DashboardPage';
+import Loader from './Loader/Loader';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const getLoadingData = async () => {
+      try {
+        setIsLoading(true);
+        setIsError(false);
+      } catch (error) {
+        setIsError(true);
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getLoadingData();
+  });
+
   return (
-    <>
+    <div>
       <Toaster />
       <Layout>
         <Balance />
@@ -46,7 +67,10 @@ const App = () => {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
-    </>
+
+      {isLoading && <Loader />}
+      {isError && <h2>Something went wrong! Try again...</h2>}
+    </div>
   );
 };
 

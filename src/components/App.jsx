910a34a1b0e1a303c9af882821cf68ceae70c932
Inka <innamakarec349@gmail.com>
2025-04-24 20:34:32 +0400

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 
 import RegistrationPage from '../pages/RegistrationPage/RegistrationPage';
 import LoginPage from '../pages/LoginPage/LoginPage';
@@ -12,29 +12,36 @@ import NotFound from '../pages/NotFound';
 import HomeTab from '../pages/HomeTab/HomeTab';
 import Balance from './Balance/Balance';
 import DashboardPage from '../pages/DashboardPage/DashboardPage';
-import Loader from './Loader/Loader';
+// import Loader from './Loader/Loader';
 
 import LogOut from '../components/LogOut/LogOut'; // потом убрать
+// import { refreshThunk } from '../redux/auth/operations';
+// import { useDispatch } from 'react-redux';
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
+  // const dispatch = useDispatch();
+  // useEffect(() => {
+  //   dispatch(refreshThunk());
+  // }, [dispatch]);
 
-  useEffect(() => {
-    const getLoadingData = async () => {
-      try {
-        setIsLoading(true);
-        setIsError(false);
-      } catch (error) {
-        setIsError(true);
-        console.log(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [isError, setIsError] = useState(false);
 
-    getLoadingData();
-  });
+  // useEffect(() => {
+  //   const getLoadingData = async () => {
+  //     try {
+  //       setIsLoading(true);
+  //       setIsError(false);
+  //     } catch (error) {
+  //       setIsError(true);
+  //       console.log(error);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   };
+
+  //   getLoadingData();
+  // });
 
   const [showLogout, setShowLogout] = useState(false); // убрать потом
 
@@ -85,8 +92,8 @@ const App = () => {
         </Routes>
       </Layout>
 
-      {isLoading && <Loader />}
-      {isError && <h2>Something went wrong! Try again...</h2>}
+      {/* {isLoading && <Loader />}
+      {isError && <h2>Something went wrong! Try again...</h2>} */}
     </div>
   );
 };

90a27a3e13cec4f2860a7f1f31aed877404b660d
Alena-Solo <bazarkina.alena2020@gmail.com>
2025-04-25 16:01:55 +0200

    update

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -7,8 +7,8 @@ const Loader = () => {
   if (!isLoading) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center z-50">
-      <ClipLoader color="#734aef" size={70} />
+    <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-50">
+      <ClipLoader color="#734aef" loading={true} size={80} />
     </div>
   );
 };

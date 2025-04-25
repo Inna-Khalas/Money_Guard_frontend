9e22f155569090438a1a911296b10d74babe6926
Alina_Vitiuk <vityukalina@gmail.com>
2025-04-25 15:02:40 +0200

    header: added header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./header.module.css";
 import favicon from "../../assets/favicon.svg";
 import group from "./Group 7.svg";
+import clsx from "clsx";
+
 const Header = () => {
   return (
     <header className={styles.header}>
@@ -9,11 +11,15 @@ const Header = () => {
         <img src={favicon} alt="Logo" className={styles.logo} />
         <span className={styles.appName}>Money Guard</span>
       </div>
-      <button className={styles.profileButton}>
-        Name
-        <img src={group} alt="group" className={styles.editIcon} />
-        Exit
-      </button>
+      <div className={styles.buttonContainer}>
+        <button className={styles.profileButton}>Name</button>
+        <button className={styles.profileButton}>
+          <img src={group} alt="group" className={styles.editIcon} />
+        </button>
+        <button className={clsx(styles.profileButton, styles.hidden)}>
+          Exit
+        </button>
+      </div>
     </header>
   );
 };

64cf688c9c9bc667110ccee29193f560462ad76b
Inka <innamakarec349@gmail.com>
2025-05-05 13:28:56 +0400

    fix

--- src/pages/HomeTab/HomeTab.jsx
+++ src/pages/HomeTab/HomeTab.jsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from 'react';
 import TransactionsList from '../../components/TransactionsList/TransactionsList';
 import ButtonAddTransactions from '../../components/ButtonAddTransactions/ButtonAddTransactions';
 import Balance from '../../components/Balance/Balance';
-// import ModalEditTransaction from '../../components/ModalEditTransaction/ModalEditTransaction';
 import ModalAddTransaction from '../../components/ModalAddTransaction/ModalAddTransaction';
 
 import s from './HomeTab.module.css';
@@ -12,7 +11,6 @@ import s from './HomeTab.module.css';
 const HomeTab = () => {
   const { isMobile } = useMedia();
   const [isModalOpen, setIsModalOpen] = useState(false);
-  // const [editTransaction, setEditTransaction] = useState(null);
 
   useEffect(() => {
     if (isModalOpen) {
@@ -22,10 +20,6 @@ const HomeTab = () => {
     }
   }, [isModalOpen]);
 
-  // const handleEditTransaction = transaction => {
-  //   setEditTransaction(transaction);
-  // };
-
   return (
     <div className={s.homeTab}>
       {isMobile && <Balance />}
@@ -34,12 +28,6 @@ const HomeTab = () => {
       {isModalOpen && (
         <ModalAddTransaction onClose={() => setIsModalOpen(false)} />
       )}
-      {/* {editTransaction && (
-        <ModalEditTransaction
-          transaction={editTransaction}
-          onClose={() => setEditTransaction(null)}
-        />
-      )} */}
     </div>
   );
 };
